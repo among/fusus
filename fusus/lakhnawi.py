975d80b36b515b041f3402fc15b73137c54d0a87
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2020-12-10 20:46:51 +0100

    arabic lab

--- fusus/lakhnawi.py
+++ fusus/lakhnawi.py
@@ -1,7 +1,8 @@
 import collections
 import re
-from itertools import chain
+import pprint as pp
 
+from itertools import chain
 from unicodedata import name as uname
 
 from IPython.display import display, HTML, Image
@@ -10,6 +11,12 @@ import fitz
 
 from tf.core.helpers import setFromSpec
 from .parameters import SOURCE_DIR
+
+PP = pp.PrettyPrinter(indent=2)
+
+
+def pprint(x):
+    PP.pprint(x)
 
 
 NAME = "Lakhnawi"
@@ -24,6 +31,10 @@ CSS = """
     font-size: 24pt;
     direction: rtl;
 }
+p {
+    text-align: left;
+    direction: ltr;
+}
 p.r {
     text-align: right;
     direction: rtl;
@@ -32,14 +43,11 @@ p.r {
     font-family: normal, sans-serif;
     font-size: 16pt;
     direction: ltr;
+    unicode-bidi: isolate-override;
 }
 .c {
     font-family: monospace;
     font-size: 12pt;
-    direction: ltr;
-}
-p.l {
-    text-align: left;
     direction: ltr;
 }
 .p {
@@ -48,6 +56,9 @@ p.l {
     font-weight: bold;
     background-color: yellow;
     direction: ltr;
+}
+td {
+    text-align: left ! important;
 }
 </style>
 """
@@ -62,38 +73,71 @@ SEMITIC_RANGES = (
     ("fb50", "fdfd"),
     ("fe70", "fefc"),
     ("0591", "05f4"),
+    ("206a", "206f"),
     ("fb1d", "fb4f"),
 )
 
 NO_SPACING_RANGES = (("064b", "0652"),)
 
+BRACKET_RANGES = (
+    ("0028", "0029"),  # parentheses
+    ("003c", "003c"),  # less than
+    ("003e", "003e"),  # greater than
+    ("005b", "005b"),  # left  sq bracket
+    ("005d", "005d"),  # right sq bracket
+    ("007b", "007b"),  # left  brace
+    ("007d", "007d"),  # right brace
+    ("00ab", "00ab"),  # left  guillemet double
+    ("00bb", "00bb"),  # right guillemet double
+    ("2018", "201d"),  # qutation marks directed
+    ("2039", "203a"),  # guillemets single
+    ("2045", "2046"),  # sqare brackets with quill
+    ("204c", "204d"),  # bullets directed
+)
+
+DIRECTION_RANGES = (
+    ("202a", "202e"),  # control writing direction
+    ("2066", "2069"),  # control writing direction
+)
+
 NEUTRAL_DIRECTION_RANGES = (
-    ("0020", "002f"),
-    ("003a", "0040"),
-    ("005b", "0060"),
-    ("007b", "00b1"),
-    ("00b4", "00b8"),
-    ("00ba", "00bf"),
-    ("00f7", "00f7"),
-    ("02d8", "02df"),
-    ("02e5", "0362"),
-    ("2000", "206f"),
+    ("0020", "0020"),
+    ("2000", "2017"),
+    ("201e", "2029"),
+    ("202f", "2038"),
+    ("203b", "2044"),
+    ("204a", "2044"),
+    ("2056", "2064"),
+    ("201e", "206f"),
 )
 
 REPLACE_DEF = """
 0627+e815 => 0623+064e : ALIF-HAMZA + FATA
 e80e+e807 => fefc      : LAM + ALEF LIGATURE
-e821      =>           : TATWEEL (short)
+e821      => 0640      : TATWEEL (short)
+0640+e82b => 0670+640  : TATWEEL + SUPERSCRIPT ALEF
 e825      => 064e      : FATHA (high)
 e826      => 064f      : DAMMA (high)
+e827      => 0651      : SHADDA (high)
 e828      => 0652      : SUKUN (high)
-e830      => 064e+0651 : SHADDA+FATHA LIG => non-isolated, separate chars
+e830      => 064e+0651 : FATHA+SHADDA LIG => non-isolated, separate chars
+e831      => 064f+0651 : DAMMA+SHADDA LIG => non-isolated, separate chars
+e845      => 0655+0650 : HAMZA+KASRA (low)
 e8e8      => 064e      : FATHA (mid)
 e864      => 0650      : KASRA (low)
 e8df      => 0650      : KASRA (low)
 e8e9      => 064f      : DAMMA
+e8eb      => 0652      : SUKUN
 fc60      => 064e+0651 : SHADDA+FATHA LIG => non-isolated, separate chars
 """
+
+
+def uName(c):
+    try:
+        un = uname(c)
+    except Exception:
+        un = "NO NAME"
+    return un
 
 
 def getSetFromRanges(rngs):
@@ -118,7 +162,7 @@ def getDictFromDef(defs):
         vals = vals.split("+")
         repls = [int(repl, base=16) for repl in repls.split("+")] if repls else []
         if len(vals) == 1:
-            result[int(vals[0], base=16)] = repls
+            result.setdefault(int(vals[0], base=16), {})[None] = repls
         elif len(vals) == 2:
             result.setdefault(int(vals[0], base=16), {})[int(vals[1], base=16)] = repls
         else:
@@ -129,21 +173,32 @@ def getDictFromDef(defs):
 
 U_LINE_RE = re.compile(r"""^U\+([0-9a-f]{4})([0-9a-f ]*)$""", re.I)
 HEX_RE = re.compile(r"""^[0-9a-f]{4}$""", re.I)
+PUA_RE = re.compile(r"""⌊([^⌋]*)⌋""")
 
 
 def showString(x):
-    display(HTML(f"""<span class="r">{x}</span>"""))
+    display(HTML(f"""<p><span class="r"> {x} </span></p>"""))
     for c in x:
+
         display(
             HTML(
                 f"""<p><span class="r">{c}</span>&nbsp;&nbsp;"""
-                f"""<span class="c">{ord(c):>04x} {uname(c)}</span></p>"""
+                f"""<span class="c">{ord(c):>04x} {uName(c)}</span></p>"""
             )
         )
 
 
 class Lakhnawi:
     def __init__(self):
+        self.getCharConfig()
+        self.doc = fitz.open(SOURCE)
+        self.text = {}
+        self.lines = {}
+
+    def setStyle(self):
+        display(HTML(CSS))
+
+    def getCharConfig(self):
         self.puas = getSetFromRanges(PUA_RANGES)
         self.semis = getSetFromRanges(SEMITIC_RANGES)
         self.neutrals = getSetFromRanges(NEUTRAL_DIRECTION_RANGES)
@@ -151,11 +206,6 @@ class Lakhnawi:
         self.replace = getDictFromDef(REPLACE_DEF)
         self.rls = self.puas | self.semis
         self.getCharInfo()
-        self.doc = fitz.open(SOURCE)
-        self.text = {}
-
-    def setStyle(self):
-        display(HTML(CSS))
 
     def getCharInfo(self):
         self.doubles = {}
@@ -217,7 +267,10 @@ class Lakhnawi:
             pix = page.getPixmap(matrix=fitz.Matrix(4, 4), alpha=False)
             display(Image(data=pix.getPNGData(), format="png"))
 
-    def getPages(self, pageNumSpec):
+    def getPages(self, pageNumSpec, refreshConfig=False):
+        if refreshConfig:
+            self.getCharConfig()
+
         for pageNum in self.parsePageNums(pageNumSpec):
             self.pageNum = pageNum
             doc = self.doc
@@ -244,45 +297,82 @@ class Lakhnawi:
                 html.append(f"""<p class="r">{self.htmlLine(line)}</p>\n""")
             display(HTML("".join(html)))
 
-    def showInfo(self, pageNumSpec):
+    def showInfo(self, pageNumSpec, onlyPuas=False, long=False):
         pageNums = self.parsePageNums(pageNumSpec)
-        text = self.text
         puas = self.puas
+        rls = self.rls
+        text = self.text
 
-        puasOut = collections.defaultdict(collections.Counter)
+        charsOut = collections.defaultdict(collections.Counter)
 
         texts = {pageNum: text[pageNum] for pageNum in pageNums if pageNum in text}
 
         for (pageNum, pageText) in texts.items():
             for line in pageText:
-                for char in line:
-                    c = char[-1]
-                    for d in c:
-                        ud = ord(d)
-                        if ud in puas:
-                            puasOut[f"{ud:>04x}"][pageNum] += 1
+                for span in line:
+                    thesePuas = PUA_RE.findall(span[1])
+                    for pua in thesePuas:
+                        charsOut[chr(int(pua, base=16))][pageNum] += 1
+                    if not onlyPuas:
+                        rest = PUA_RE.sub("", span[1])
+                        for c in rest:
+                            charsOut[c][pageNum] += 1
 
-        totalChars = len(puasOut)
-        totalPages = len(set(chain.from_iterable(puasOut.values())))
-        totalOccs = sum(sum(pns.values()) for pns in puasOut.values())
+        totalChars = len(charsOut)
+        totalPages = len(set(chain.from_iterable(charsOut.values())))
+        totalOccs = sum(sum(pns.values()) for pns in charsOut.values())
 
         charRep = "character" + ("" if totalChars == 1 else "s")
         occRep = "occurence" + ("" if totalOccs == 1 else "s")
         pageRep = "page" + ("" if totalPages == 1 else "s")
 
-        print(
-            f"{totalChars} private use {charRep} in text"
-            f" in {totalOccs} {occRep} on {totalPages} {pageRep}"
+        label = "private use " if onlyPuas else ""
+
+        html = []
+        html.append(
+            f"""
+<p><b>{totalChars} {label}{charRep} in {totalOccs} {occRep}
+on {totalPages} {pageRep}</b></p>
+<table>
+"""
         )
-        for xc in sorted(puasOut):
-            pageNums = puasOut[xc]
+        for c in sorted(charsOut):
+            xc = ord(c)
+            ccode = f"""<span class="{"p" if xc in puas else "c"}">{xc:>04x}</span>"""
+            crep = (
+                ""
+                if xc in puas
+                else f"""<span class="{"r" if xc in rls else "l"}">{c}"""
+            )
+            cname = "" if xc in puas else f"""<span class="c">{uName(c)}</span>"""
+
+            pageNums = charsOut[c]
             nPageNums = len(pageNums)
             pageRep = "page" + ("" if nPageNums == 1 else "s")
             thistotal = sum(pageNums.values())
-            print(f"{xc}: {thistotal} x on {nPageNums} {pageRep}")
-            for pn in sorted(pageNums):
-                occs = pageNums[pn]
-                print(f"\t\t\tpage {pn:>3}: {occs:>3} x")
+            html.append(
+                f"""
+<tr>
+    <td>{ccode}</td>
+    <td>{crep}</td>
+    <td>{cname}</td>
+    <td><b>{thistotal}</b> on <i>{nPageNums}</i> {pageRep}</td>
+</tr>
+"""
+            )
+            if long:
+                for pn in sorted(pageNums):
+                    occs = pageNums[pn]
+                    html.append(
+                        f"""
+<tr>
+    <td></td>
+    <td><i>page {pn:>3}</i>: <b>{occs:>3}</b></td>
+</tr>
+"""
+                    )
+        html.append("</table>")
+        display(HTML("".join(html)))
 
     def collectPage(self, data):
         doubles = self.doubles
@@ -298,10 +388,7 @@ class Lakhnawi:
             box = tuple(int(round(x * 10)) for x in prevChar["bbox"])
             c = prevChar["c"]
             uc = ord(c)
-            try:
-                un = uname(c)
-            except Exception:
-                un = "NO NAME"
+            un = uName(c)
             chars.append(
                 (
                     *box,
@@ -331,6 +418,8 @@ class Lakhnawi:
                     c = data["c"]
                     uc = ord(c)
                     skip = False
+                    if c == " ":
+                        skip = True
 
                     if prevChar is not None:
                         pc = prevChar["c"]
@@ -361,72 +450,77 @@ class Lakhnawi:
         for char in sorted(chars, key=lambda c: (clusterKeyCharV(c), -keyCharH(c))):
             k = clusterKeyCharV(char)
             lines.setdefault(k, []).append(char)
+
+        self.lines[pageNum] = tuple(lines.values())
         self.text[pageNum] = tuple(self.trimLine(line) for line in lines.values())
 
     def trimLine(self, chars):
         replace = self.replace
+        puas = self.puas
+        nospacings = self.nospacings
+        neutrals = self.neutrals
+        rls = self.rls
 
-        result = []
+        stageOne = []
 
         for char in chars:
             char = list(char)
             c = char[-1]
             uc = ord(c)
 
-            if result:
-                prevChar = result[-1]
+            if stageOne:
+                prevChar = stageOne[-1]
                 pc = prevChar[-1]
                 if len(pc) == 1:
                     puc = ord(prevChar[-1])
                     if puc in replace:
                         repls = replace[puc]
-                        if type(repls) is dict and uc in repls:
+                        if uc in repls:
                             ucs = repls[uc]
                             if len(ucs) == 0:
-                                result.pop()
+                                stageOne[-1][-1] = ""
+                                char[-1] = ""
+                                stageOne.append(char)
                                 continue
 
                             (puc, *ucs) = ucs
-                            result[-1][-1] = chr(puc)
+                            stageOne[-1][-1] = chr(puc)
                             char[-1] = "".join(chr(u) for u in ucs)
-                            result.append(char)
+                            stageOne.append(char)
                             continue
 
             if uc in replace:
                 repls = replace[uc]
-                if type(repls) is list:
+                if None in repls:
+                    repls = repls[None]
                     if len(repls) == 0:
+                        char[-1] = ""
+                        stageOne.append(char)
                         continue
 
                     char[-1] = "".join(chr(u) for u in repls)
-                    result.append(char)
+                    stageOne.append(char)
                     continue
 
-            result.append(char)
+            stageOne.append(char)
 
-        return result
-
-    def plainLine(self, chars):
-        return "".join(c[-1] for c in chars)
-
-    def htmlLine(self, chars):
-        puas = self.puas
-        nospacings = self.nospacings
-        neutrals = self.neutrals
-        rls = self.rls
-
-        result = []
-        text = []
+        stageTwo = []
         prevLeft = None
         prevDir = "r"
+        chars = []
 
-        for char in chars:
+        def addChars():
+            if chars:
+                charsRep = "".join(chars if prevDir == "r" else reversed(chars))
+                stageTwo.append((prevDir, charsRep))
+
+        for char in stageOne:
             left = int(round(char[0]))
             right = int(round(char[2]))
 
             if prevLeft is not None:
                 if prevLeft - right >= 25:
-                    text.append(" ")
+                    chars.append(" ")
 
             c = char[-1]
             if c == "":
@@ -441,20 +535,30 @@ class Lakhnawi:
             thisDir = prevDir if uc in neutrals else "r" if uc in rls else "l"
 
             if prevDir != thisDir:
-                if text:
-                    result.append(f"""<span class="{prevDir}">{"".join(text)}</span>""")
-                text = []
+                addChars()
+                chars = []
                 prevDir = thisDir
 
             rep = c
             for d in c:
                 ud = ord(d)
                 if ud in puas:
-                    rep = f"""<span class="p">[{ud:>04x}]</span>"""
-            text.append(rep)
+                    rep = f"""⌊{ud:>04x}⌋"""
+            chars.append(rep)
 
-        if text:
-            result.append(f"""<span class="{prevDir}">{"".join(text)}</span>""")
+        addChars()
+
+        return stageTwo
+
+    def plainLine(self, spans):
+        return "".join("".join(span[1]) for span in spans)
+
+    def htmlLine(self, spans):
+        result = []
+
+        for (textDir, string) in spans:
+            rep = string.replace("⌊", """<span class="p">""").replace("⌋", "</span>")
+            result.append(f"""<span class="{textDir}">{rep}</span>""")
 
         return "".join(result)
 
