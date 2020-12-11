6a70afbc032ec067cb59d2effed925566647cc99
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2020-12-11 17:47:52 +0100

    mapping private use characters in full swing

--- fusus/lakhnawi.py
+++ fusus/lakhnawi.py
@@ -28,8 +28,21 @@ CSS = """
 <style>
 .r {
     font-family: normal, sans-serif;
-    font-size: 24pt;
+    font-size: xx-large;
     direction: rtl;
+}
+.rc, .lc {
+    font-family: normal, sans-serif;
+    font-size: xx-large;
+    background-color: white;
+    border: 2pt solid #ffcccc;
+}
+.rc {
+    direction: rtl;
+}
+.lc {
+    direction: ltr;
+    unicode-bidi: isolate-override;
 }
 p {
     text-align: left;
@@ -41,24 +54,52 @@ p.r {
 }
 .l {
     font-family: normal, sans-serif;
-    font-size: 16pt;
+    font-size: medium;
     direction: ltr;
     unicode-bidi: isolate-override;
 }
 .c {
     font-family: monospace;
-    font-size: 12pt;
+    font-size: x-small;
     direction: ltr;
 }
 .p {
     font-family: monospace;
-    font-size: 12pt;
+    font-size: medium;
     font-weight: bold;
     background-color: yellow;
     direction: ltr;
 }
+.lrg {
+    font-size: xx-large;
+    font-weight: bold;
+}
 td {
     text-align: left ! important;
+}
+div.cn {
+    text-align: center
+}
+span.cni {
+    background-color: #eeeeee;
+    padding-top: 4pt;
+    padding-bottom: 4pt;
+    padding-left: 8pt;
+    padding-right: 8pt;
+    border: 2pt solid #66aaaa;
+    display: inline-block;
+}
+div.ch {
+    border: 2pt solid #cccccc;
+    background-color: #ddffff;
+    display: inline-flex;
+    flex-flow: column nowrap;
+    max-width: 10em;
+}
+div.sr {
+    display: flex;
+    flex-flow: row wrap;
+    direction: rtl;
 }
 </style>
 """
@@ -112,23 +153,36 @@ NEUTRAL_DIRECTION_RANGES = (
 )
 
 REPLACE_DEF = """
-0627+e815 => 0623+064e : ALIF-HAMZA + FATA
-e80e+e807 => fefc      : LAM + ALEF LIGATURE
-e821      => 0640      : TATWEEL (short)
-0640+e82b => 0670+640  : TATWEEL + SUPERSCRIPT ALEF
-e825      => 064e      : FATHA (high)
-e826      => 064f      : DAMMA (high)
-e827      => 0651      : SHADDA (high)
-e828      => 0652      : SUKUN (high)
-e830      => 064e+0651 : FATHA+SHADDA LIG => non-isolated, separate chars
-e831      => 064f+0651 : DAMMA+SHADDA LIG => non-isolated, separate chars
-e845      => 0655+0650 : HAMZA+KASRA (low)
-e8e8      => 064e      : FATHA (mid)
-e864      => 0650      : KASRA (low)
-e8df      => 0650      : KASRA (low)
-e8e9      => 064f      : DAMMA
-e8eb      => 0652      : SUKUN
-fc60      => 064e+0651 : SHADDA+FATHA LIG => non-isolated, separate chars
+0627+0648+e815 => 0623+064e+0648 : ALIF+x+HAMZA|FATA => ALIF|HAMZA+FATA+x
+0627+e80a+e815 => 0623+064e+e80a : ALIF+x+HAMZA|FATA => ALIF|HAMZA+FATA+x
+0627+fe97+e816 => 0623+064f+fe97 : ALIF+x+HAMZA|DAMMA => ALIF|HAMZA+DAMMA+x
+0627+e815      => 0623+064e      : ALIF+HAMZA|FATA => ALIF|HAMZA+FATA
+0627+e816      => 0623+064f      : ALIF+HAMZA|DAMMA => ALIF|HAMZA+DAMMA
+e80a+e806      => fefb           : LAM/ALEF LIGATURE (isolated)
+e80e+e807      => fefc           : LAM/ALEF LIGATURE (final)
+e821+e82b      => 0670+0640      : TATWEEL+SUPERSCRIPT ALEF
+e821           => 0640           : TATWEEL (short)
+e823           => 064b           : FATHATAN (high)
+e825           => 064e           : FATHA (high)
+e826           => 064f           : DAMMA (high)
+e827           => 0651           : SHADDA (high)
+e828           => 0652           : SUKUN (high)
+e82b           => 0670           : SUPERSCRIPT ALEF
+e830           => fcf2           : FATHA|SHADDA medial
+e831           => fcf3           : DAMMA|SHADDA medial
+e832           => fcf4           : SHADDA|KASRA medial
+e833           => 0651+0670      : SHADDA+SUPERSCRIPT ALEF
+e845           => 0655+0650      : HAMZA+KASRA (low)
+e864           => 0650           : KASRA (low)
+e887           => 064d           : KASRATAN (low)
+e888           => 0650           : KASRA (low)
+e8d4           => fee0           : LAM (medial)
+e8df           => 0650           : KASRA (low)
+e8e8           => 064e           : FATHA (mid)
+e8e9           => 064f           : DAMMA
+e8ea           => 0651           : SHADDA (mid)
+e8eb           => 0652           : SUKUN
+fc60           => 064e+0651      : SHADDA|FATHA LIG => SHADDA+FATHA
 """
 
 
@@ -148,44 +202,56 @@ def getSetFromRanges(rngs):
     return result
 
 
+def numericString(s):
+    return "".join(f"⌊{ord(c):>04x}⌋" for c in s)
+
+
 REPLACE_RE = re.compile(r"""^([0-9a-z+]+)\s*=>\s*([0-9a-z+]*)\s*:\s*(.*)$""", re.I)
 
 
 def getDictFromDef(defs):
-    result = {}
+    rules = []
     for line in defs.strip().split("\n"):
+        parts = line.split("#", maxsplit=1)
+        if len(parts) > 1:
+            line = parts[0]
+        line = line.strip()
+        if not line:
+            continue
         match = REPLACE_RE.match(line)
         if not match:
             print(f"MALFORMED REPLACE DEF: {line}")
             continue
         (vals, repls, comment) = match.group(1, 2, 3)
-        vals = vals.split("+")
-        repls = [int(repl, base=16) for repl in repls.split("+")] if repls else []
-        if len(vals) == 1:
-            result.setdefault(int(vals[0], base=16), {})[None] = repls
-        elif len(vals) == 2:
-            result.setdefault(int(vals[0], base=16), {})[int(vals[1], base=16)] = repls
-        else:
-            print(f"MORE THAN 2 SOURCE CHARS IN REPLACE DEF: {line}")
-            continue
+        vals = tuple(chr(int(val, base=16)) for val in vals.split("+"))
+        repls = (
+            tuple(chr(int(repl, base=16)) for repl in repls.split("+")) if repls else ()
+        )
+        rules.append((vals, repls))
+
+    result = {}
+
+    for (vals, repls) in sorted(rules, key=lambda x: (-len(x[0]), x[0])):
+        result.setdefault(vals[0], []).append((vals[1:], repls))
+
     return result
+
+
+def parseNums(numSpec):
+    return (
+        None
+        if not numSpec
+        else [numSpec]
+        if type(numSpec) is int
+        else setFromSpec(numSpec)
+        if type(numSpec) is str
+        else list(numSpec)
+    )
 
 
 U_LINE_RE = re.compile(r"""^U\+([0-9a-f]{4})([0-9a-f ]*)$""", re.I)
 HEX_RE = re.compile(r"""^[0-9a-f]{4}$""", re.I)
 PUA_RE = re.compile(r"""⌊([^⌋]*)⌋""")
-
-
-def showString(x):
-    display(HTML(f"""<p><span class="r"> {x} </span></p>"""))
-    for c in x:
-
-        display(
-            HTML(
-                f"""<p><span class="r">{c}</span>&nbsp;&nbsp;"""
-                f"""<span class="c">{ord(c):>04x} {uName(c)}</span></p>"""
-            )
-        )
 
 
 class Lakhnawi:
@@ -245,6 +311,60 @@ class Lakhnawi:
                         else:
                             doubles[nSecond] = nMain
 
+    def showChar(self, c):
+        puas = self.puas
+        rls = self.rls
+
+        xc = ord(c)
+        ccode = f"""<span class="{"p" if xc in puas else "c"}">{xc:>04x}</span>"""
+        crep = (
+            "??"
+            if xc in puas
+            else f"""<span class="{"rc" if xc in rls else "lc"}">{c}"""
+        )
+        cname = "" if xc in puas else f"""<span class="c">{uName(c)}</span>"""
+
+        return f"""
+<div class="ch">
+    <div class="cn">{ccode}</div>
+    <div class="cn"><span class="cni">{crep}</span></div>
+    <div class="cn">{cname}</div>
+</div>
+"""
+
+    def showString(self, s):
+        display(HTML(f"""<p><span class="r">{s}</span></p>"""))
+        display(
+            HTML(
+                """<div class="sr">"""
+                + ("".join(self.showChar(c) for c in s) + "</div>")
+            )
+        )
+
+    def showReplacements(self):
+        replace = self.replace
+
+        html = []
+        html.append("<table>")
+
+        for val in sorted(replace):
+            rules = replace[val]
+            for (vals, repls) in rules:
+                valRep = "".join(self.showChar(c) for c in (val,) + vals)
+                replRep = "".join(self.showChar(c) for c in repls)
+                html.append(
+                    f"""
+<tr>
+    <td>{valRep}</td>
+    <td><span class="lrg">⇒</span></td>
+    <td>{replRep}</td>
+</tr>
+"""
+                )
+
+        html.append("<table>")
+        display(HTML("".join(html)))
+
     def parsePageNums(self, pageNumSpec):
         doc = self.doc
         pageNums = (
@@ -267,7 +387,7 @@ class Lakhnawi:
             pix = page.getPixmap(matrix=fitz.Matrix(4, 4), alpha=False)
             display(Image(data=pix.getPNGData(), format="png"))
 
-    def getPages(self, pageNumSpec, refreshConfig=False):
+    def getPages(self, pageNumSpec, refreshConfig=False, debug=False):
         if refreshConfig:
             self.getCharConfig()
 
@@ -278,7 +398,7 @@ class Lakhnawi:
 
             textPage = page.getTextPage()
             data = textPage.extractRAWDICT()
-            self.collectPage(data)
+            self.collectPage(data, debug=debug)
 
     def plainPages(self, pageNumSpec):
         for pageNum in self.parsePageNums(pageNumSpec):
@@ -297,10 +417,83 @@ class Lakhnawi:
                 html.append(f"""<p class="r">{self.htmlLine(line)}</p>\n""")
             display(HTML("".join(html)))
 
-    def showInfo(self, pageNumSpec, onlyPuas=False, long=False):
+    def showLines(self, pageNumSpec, lineSpec, charSpec):
+        lines = self.lines
         pageNums = self.parsePageNums(pageNumSpec)
-        puas = self.puas
-        rls = self.rls
+        lineNums = parseNums(lineSpec)
+        charNums = parseNums(charSpec)
+        if charNums is not None:
+            charNums = set(charNums)
+
+        myLines = {pageNum: lines[pageNum] for pageNum in pageNums if pageNum in lines}
+
+        html = []
+        html.append("<table>")
+        html.append(
+            """
+<tr>
+    <th>seq</th>
+    <th>bottom</th>
+    <th>height</th>
+    <th>width</th>
+    <th>shift</th>
+    <th>after</th>
+    <th>font</th>
+    <th>size</th>
+    <th>char</th>
+</tr>
+"""
+        )
+
+        for (pageNum, pageLines) in myLines.items():
+            myLineNums = (
+                range(1, len(pageLines) + 1)
+                if lineNums is None
+                else [ln for ln in lineNums if 0 < ln <= len(pageLines)]
+            )
+            for ln in myLineNums:
+                chars = pageLines[ln - 1]
+                nChars = len(chars)
+                html.append(
+                    f"""
+<tr>
+    <th colspan=3>page {pageNum}</th>
+    <th colspan=2>line {ln}</th>
+    <th colspan=3>{nChars} characters</th>
+</tr>
+"""
+                )
+                for (i, char) in enumerate(chars):
+                    (le, to, ri, bo, font, size, c) = char
+                    if charNums is not None and i + 1 not in charNums:
+                        continue
+                    after = "x" if i == nChars - 1 else le - chars[i + 1][2]
+                    shift = (
+                        "x"
+                        if i == nChars - 1
+                        else keyCharH(char) - keyCharH(chars[i + 1])
+                    )
+                    html.append(
+                        f"""
+<tr>
+    <td><b>{i + 1}</b></td>
+    <td>{bo}</td>
+    <td>{bo - to}</td>
+    <td>{ri - le}</td>
+    <td>{shift}</td>
+    <td>{after}</td>
+    <td>{font}</td>
+    <td>{size}pt</td>
+    <td>{"".join(self.showChar(x) for x in c)}</td>
+</tr>
+"""
+                    )
+
+        html.append("</table>")
+        display(HTML("".join(html)))
+
+    def showUsedChars(self, pageNumSpec, onlyPuas=False, long=False):
+        pageNums = self.parsePageNums(pageNumSpec)
         text = self.text
 
         charsOut = collections.defaultdict(collections.Counter)
@@ -337,15 +530,6 @@ on {totalPages} {pageRep}</b></p>
 """
         )
         for c in sorted(charsOut):
-            xc = ord(c)
-            ccode = f"""<span class="{"p" if xc in puas else "c"}">{xc:>04x}</span>"""
-            crep = (
-                ""
-                if xc in puas
-                else f"""<span class="{"r" if xc in rls else "l"}">{c}"""
-            )
-            cname = "" if xc in puas else f"""<span class="c">{uName(c)}</span>"""
-
             pageNums = charsOut[c]
             nPageNums = len(pageNums)
             pageRep = "page" + ("" if nPageNums == 1 else "s")
@@ -353,9 +537,7 @@ on {totalPages} {pageRep}</b></p>
             html.append(
                 f"""
 <tr>
-    <td>{ccode}</td>
-    <td>{crep}</td>
-    <td>{cname}</td>
+    <td>{self.showChar(c)}</td>
     <td><b>{thistotal}</b> on <i>{nPageNums}</i> {pageRep}</td>
 </tr>
 """
@@ -374,9 +556,8 @@ on {totalPages} {pageRep}</b></p>
         html.append("</table>")
         display(HTML("".join(html)))
 
-    def collectPage(self, data):
+    def collectPage(self, data, debug=False):
         doubles = self.doubles
-        puas = self.puas
         pageNum = self.pageNum
 
         chars = []
@@ -387,15 +568,11 @@ on {totalPages} {pageRep}</b></p>
         def addChar():
             box = tuple(int(round(x * 10)) for x in prevChar["bbox"])
             c = prevChar["c"]
-            uc = ord(c)
-            un = uName(c)
             chars.append(
                 (
                     *box,
                     prevFont,
                     prevSize,
-                    f"{uc:>04x}",
-                    "PRIVATE" if uc in puas else un,
                     c,
                 )
             )
@@ -449,78 +626,80 @@ on {totalPages} {pageRep}</b></p>
         lines = {}
         for char in sorted(chars, key=lambda c: (clusterKeyCharV(c), -keyCharH(c))):
             k = clusterKeyCharV(char)
-            lines.setdefault(k, []).append(char)
+            lines.setdefault(k, []).append(list(char))
 
         self.lines[pageNum] = tuple(lines.values())
-        self.text[pageNum] = tuple(self.trimLine(line) for line in lines.values())
+        self.text[pageNum] = tuple(
+            self.trimLine(line, debug=debug) for line in lines.values()
+        )
 
-    def trimLine(self, chars):
+    def trimLine(self, chars, debug=False):
         replace = self.replace
         puas = self.puas
         nospacings = self.nospacings
         neutrals = self.neutrals
         rls = self.rls
 
-        stageOne = []
+        nChars = len(chars)
 
-        for char in chars:
-            char = list(char)
+        for (i, char) in enumerate(chars):
             c = char[-1]
-            uc = ord(c)
+            if c in replace:
+                rules = replace[c]
+                for (vals, repls) in rules:
+                    if debug:
+                        ruleRep = (
+                            f"""{numericString(c)}{numericString("".join(vals)):<20}"""
+                            """ ⇒ """
+                            f"""{numericString("".join(repls)):<20}"""
+                        )
 
-            if stageOne:
-                prevChar = stageOne[-1]
-                pc = prevChar[-1]
-                if len(pc) == 1:
-                    puc = ord(prevChar[-1])
-                    if puc in replace:
-                        repls = replace[puc]
-                        if uc in repls:
-                            ucs = repls[uc]
-                            if len(ucs) == 0:
-                                stageOne[-1][-1] = ""
-                                char[-1] = ""
-                                stageOne.append(char)
-                                continue
-
-                            (puc, *ucs) = ucs
-                            stageOne[-1][-1] = chr(puc)
-                            char[-1] = "".join(chr(u) for u in ucs)
-                            stageOne.append(char)
-                            continue
-
-            if uc in replace:
-                repls = replace[uc]
-                if None in repls:
-                    repls = repls[None]
-                    if len(repls) == 0:
-                        char[-1] = ""
-                        stageOne.append(char)
+                    nVals = len(vals)
+                    if i + nVals >= nChars:
+                        if debug:
+                            print(f"{i:>4} * {ruleRep}")
+                        continue
+                    if not all(vals[j] == chars[i + j + 1][-1] for j in range(nVals)):
+                        if debug:
+                            print(f"{i:>4} x {ruleRep}")
                         continue
 
-                    char[-1] = "".join(chr(u) for u in repls)
-                    stageOne.append(char)
-                    continue
+                    if len(repls) == 0:
+                        chars[i][-1] = ""
+                    else:
+                        chars[i][-1] = repls[0]
+                        repls = repls[1:]
+                        nRepls = len(repls)
 
-            stageOne.append(char)
+                        for (j, val) in enumerate(vals):
+                            chars[i + j + 1][-1] = repls[j] if j < nRepls else ""
 
-        stageTwo = []
+                    if nRepls > nVals:
+                        chars[i + nVals][-1] += "".join(repls[nVals:])
+                    if debug:
+                        resRep = "".join(
+                            f"{numericString(c[-1])}" for c in chars[i : i + nVals + 1]
+                        )
+                        print(f"{i:>4} V {ruleRep}: {resRep}")
+                    break
+
+        result = []
         prevLeft = None
         prevDir = "r"
-        chars = []
+        outChars = []
 
         def addChars():
-            if chars:
-                charsRep = "".join(chars if prevDir == "r" else reversed(chars))
-                stageTwo.append((prevDir, charsRep))
+            if outChars:
+                charsRep = "".join(outChars if prevDir == "r" else reversed(outChars))
+                result.append((prevDir, charsRep))
 
-        for char in stageOne:
+        for char in chars:
             left = int(round(char[0]))
             right = int(round(char[2]))
 
             if prevLeft is not None:
                 if prevLeft - right >= 25:
-                    chars.append(" ")
+                    outChars.append(" ")
 
             c = char[-1]
             if c == "":
@@ -536,7 +715,7 @@ on {totalPages} {pageRep}</b></p>
 
             if prevDir != thisDir:
                 addChars()
-                chars = []
+                outChars = []
                 prevDir = thisDir
 
             rep = c
@@ -544,11 +723,11 @@ on {totalPages} {pageRep}</b></p>
                 ud = ord(d)
                 if ud in puas:
                     rep = f"""⌊{ud:>04x}⌋"""
-            chars.append(rep)
+            outChars.append(rep)
 
         addChars()
 
-        return stageTwo
+        return result
 
     def plainLine(self, spans):
         return "".join("".join(span[1]) for span in spans)
@@ -569,6 +748,8 @@ def keyCharV(char):
 
 def keyCharH(char):
     return char[2]
+    # return char[0] + (char[2] - char[0]) / 2
+    # return char[0]
 
 
 def clusterVert(data):
